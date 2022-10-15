bbb3a7dd40c34268ce55f36c748a22f7a1adc7de
Nikita Zhiznevskiy <nzhigulevskiy@bk.ru>
2022-10-15 16:52:49 +0300

    Initial commit

---
+++ src/main/java/demo/ChatWithUrSelfApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWithUrSelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWithUrSelfApplication.class, args);
+	}
+
+}
